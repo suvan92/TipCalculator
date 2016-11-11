dfec5b0dcc2bb4e221c783dc52534581bb55d983
Suvan <suvanramani@gmail.com>
2016-11-11 11:09:59 -0800

    Initial Commit

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Suvan Ramani on 2016-11-11.
+//  Copyright © 2016 suvanr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
